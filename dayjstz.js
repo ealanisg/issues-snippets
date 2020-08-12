8fe19b21faedef73a00209b4e2e9ed1230ae7f31
Ezequiel Alanis <ealanisg@gmail.com>
2020-08-12 13:59:34 -0700

    amend: minor

--- dayjstz.js
+++ dayjstz.js
@@ -7,11 +7,11 @@ dayjs.extend(utc);
 dayjs.extend(tz);
 
 const timezone = 'America/Vancouver';
-const currentDate = new Date('2020-08-08T12:00-07:00');
+const currentDate = new Date('2020-03-08T12:00-07:00');
 const nowMoment = moment(currentDate);
 const nowDayJs = dayjs(currentDate);
 
-// milliseconds for currentDate: 1596913200000
+// milliseconds for currentDate: 1583694000000
 console.log(nowDayJs.valueOf() === nowMoment.valueOf()); // -> true
 
 // timezone
@@ -22,11 +22,11 @@ console.log(tzDayJs.format() === tzMoment.format()); // -> true
 // daylight
 // https://www.timetemperature.com/canada/daylight_saving_time_canada.shtml
 const plainDate = currentDate.toISOString().slice(0, 10); // 2020-08-08
-const dlMoment = moment.tz('2020-03-08', timezone).set({
+const dlMoment = moment.tz(plainDate, timezone).set({
   hour: 12,
   minute: 30
 });
-const dlDayJs = dayjs.tz('2020-03-08', timezone)
+const dlDayJs = dayjs.tz(plainDate, timezone)
   .set('hour', 12)
   .set('minute', 30);
 console.log(dlDayJs.format() === dlMoment.format()); // -> false (true expected)
